3580f84aced630e8c34e61a80450dbec7162b92a
talhajaved <talha.javed@xprolabs.com>
2021-09-24 12:42:35 +0500

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find_by(id: params[:id])
+    if @article.nil?
+      flash.now[:alert] = "Your book was not found"
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :price, :status)
+  end
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @commnet = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
+end
