62048a421c73296e488be4574504e7b38a9c6ccf
talhajaved <talha.javed@xprolabs.com>
2021-09-30 15:42:27 +0500

    Seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 20.times do |i|
+#   Article.create(title: "Article #{i}", body: "A Article #{i} from seeds.", price: i, status: "public")
+# end
+
+Article.destroy_all
+
+50.times do |index|
+  Article.create!(title: Faker::Book.title,
+                  body: Faker::Lorem.paragraph(sentence_count: rand(2..8)),
+                  price: Faker::Number.number(digits: 2),
+                  status: "public")
+end
+
+puts "50 Articles created"
